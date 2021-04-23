ec46a91b33927e0f8c1ba85ef0c069b5d84085bc
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2021-04-23 19:57:48 +0530

    practical-11 is modified

--- practical-11/server.js
+++ practical-11/server.js
@@ -1,3 +1,31 @@
 var express = require('express');
 var app = express();
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log('Server started at Port: %s', PORT);
+});
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/html/home.html');
+});
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/html/home.html');
+});
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/html/about.html');
+});
+app.get('/signin', (req, res) => {
+    res.sendFile(__dirname + '/html/signin.html');
+    // if(req.query.username === "admin" && req.query.password === "123")
+    //     res.sendFile(__dirname + '/html/valid.html');
+    // else
+    //     res.sendFile(__dirname + '/html/valid.html');
+}); 
+app.get('/login', (req, res) => {
+    if(req.query.username === "admin" && req.query.password === "123")
+        res.sendFile(__dirname + '/html/valid.html');
+    else
+        res.sendFile(__dirname + '/html/invalid.html');
+});
+app.use("*", function(req, res){
+    res.status(404).sendFile(__dirname + '/html/404.html');
+});
