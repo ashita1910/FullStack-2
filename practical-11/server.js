c3060afd35e0407d0d93e2e1c581ab7d949b9c2f
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2021-04-25 20:36:25 +0530

    updated

--- practical-11/server.js
+++ practical-11/server.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log('Server started at Port: %s', PORT);
@@ -12,17 +15,23 @@ app.get('/home', (req, res) => {
 });
 app.get('/about', (req, res) => {
     res.sendFile(__dirname + '/html/about.html');
+}); 
+app.get('/valid', (req, res) => {
+    res.sendFile(__dirname + '/html/valid.html');
+});
+app.get('/invalid', (req, res) => {
+    res.sendFile(__dirname + '/html/invalid.html');
 });
 app.get('/signin', (req, res) => {
     res.sendFile(__dirname + '/html/signin.html');
-    auth(req, res);
-}); 
-function auth(req, res){
+});
+app.post('/signin', (req, res) => {
+    console.log(req.body);
     if(req.query.username === "admin" && req.query.password === "123")
         res.sendFile(__dirname + '/html/valid.html');
     else
         res.sendFile(__dirname + '/html/invalid.html');
-}
+});
 app.use("*", function(req, res){
     res.status(404).sendFile(__dirname + '/html/404.html');
 });
